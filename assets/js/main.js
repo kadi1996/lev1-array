5c1f6957bb91fc059614b729161bed45fdab588b
Kadi <gueneykadircan@gmail.com>
2022-05-19 12:06:30 +0200

    ready

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+// console.log("text");
+
+// ================================
+//          Lev 1-13
+// ================================
+
+// meinText1 = ["Hello", "World"];
+// meinText2 = ["Anass", "Elaine", "Eric", "Georg"];
+// meinText3 = ["Superman", "Wonderwoman", "Hulk", "Batman", "Spiderman"];
+
+// join1 = meinText1.join();
+// join2 = meinText1.join("");
+// join3 = meinText1.join(" ");
+// join4 = meinText1.join("+");
+
+// join1 = meinText2.join();
+// join2 = meinText2.join("");
+// join3 = meinText2.join(" ");
+// join4 = meinText2.join("+");
+
+// join1 = meinText3.join();
+// join2 = meinText3.join("");
+// join3 = meinText3.join(" ");
+// join4 = meinText3.join("+");
+
+// console.log(join1);
+// console.log(join2);
+// console.log(join3);
+// console.log(join4);
+
+// ================================
+//       Lev 1-1: array sort()
+// ================================
+
+// let languages = [
+//     "JavaScript",
+//     "Python",
+//     "Java",
+//     "Ruby",
+//     "PHP",
+//     "C++",
+//     "CSS",
+//     "C#",
+//     "Go",
+//     "C",
+//     "TypeScript",
+//     "Swift"
+// ];
+
+// let sortierung = languages.sort();
+// console.log(sortierung);
+
+// ================================
+//       Lev 2-2: array vertiefung
+// ================================
+
+// let numArray2 = [36, 324, 122, 62, 98, 88, 99, 1000];
+
+// let sortierung = numArray2.sort((a, b) => a - b);
+
+// console.log(sortierung);
+
+
